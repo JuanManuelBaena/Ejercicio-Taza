bf48b666bb9b01f03f26bdb1a08a2b92204de980
Juan Manuel Baena <jbaeurb3009@g.educaand.es>
2025-09-26 21:02:27 +0200

    Feat: Ejercicio taza sin colorear

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
 public class App {
+       // Background
+    public static final String BLACK_BACKGROUND = "\033[40m";  // BLACK
+    public static final String RED_BACKGROUND = "\033[41m";    // RED
+    public static final String GREEN_BACKGROUND = "\033[42m";  // GREEN
+    public static final String YELLOW_BACKGROUND = "\033[43m"; // YELLOW
+    public static final String BLUE_BACKGROUND = "\033[44m";   // BLUE
+    public static final String PURPLE_BACKGROUND = "\033[45m"; // PURPLE
+    public static final String CYAN_BACKGROUND = "\033[46m";   // CYAN
+    public static final String WHITE_BACKGROUND = "\033[47m";  // WHITE
+    public static final String RESET = "\033[0m";  // Text Reset
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        
+        
+        
+        
+        System.out.printf("%n███████████████████████████████████░████████████████████████");
+        System.out.printf("%n████████████████████████████████████████████████████████████");
+        System.out.printf("%n████████Aprende Java█████████████░██████████████████████████");
+        System.out.printf("%n████████Con Ejercicios██████████████████████████████████████");
+        System.out.printf("%n█████████████████████████████████████░██████████████████████");
+        System.out.printf("%n████████████████████████████████████████████████████████████");
+        System.out.printf("%n████████Luis José Sánchez███████▓▓▓▓▓▓▓▓▓▓▓▓████████████████");
+        System.out.printf("%n████████████████████████████████▓▓▓▓▓▓▓▓▓██▓████████████████");
+        System.out.printf("%n████████████████████████████████▓▓▓▓▓▓▓▓▓▓▓▓████████████████");
+        System.out.printf("%n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒████████████");
+        System.out.printf("%n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒████████████");
+        System.out.printf("%n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████████████");
+        System.out.printf("%n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████████████");
     }
 }
